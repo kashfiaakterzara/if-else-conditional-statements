af354c2bd9a93c35278c539e11d9a0d4f78088e8
kashfiaakterzara <kashfiaakter13@gmail.com>
2026-06-29 03:58:04 +0600

    Add files via upload

---
+++ positiveornegativecheck.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+ int num;
+ printf("Enter number: ");
+ scanf("%d", &num);
+ if(num >= 0){
+    printf("Number is positive");
+ }
+else{
+    printf("Number is negative");
+}
+
+return 0;
+}
